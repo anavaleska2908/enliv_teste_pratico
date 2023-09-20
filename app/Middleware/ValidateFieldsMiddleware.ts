fc88ee215e1097cccacae91d9d46cb3f94b7ac84
anavaleska2908 <avaleska2908@hotmail.com>
2023-09-19 21:37:17 -0300

    feature/fix: Construction of documentation. Correction of the middleware to catch the status types error.

--- app/Middleware/ValidateFieldsMiddleware.ts
+++ app/Middleware/ValidateFieldsMiddleware.ts
@@ -4,20 +4,27 @@ export default class ValidateFieldsMiddleware {
   public async handle({request, response}: HttpContextContract, next: () => Promise<void>) {
     const method = request.method();
     const body = request.body();
-    const fields = ["title","description","status"];
+    const fields = ["title", "description", "status"];
+    const enumStatusField = ["incomplete", "complete"];
     if (method === 'POST') {
       for (let i = 0; i < fields.length; i++) {
         if (!!!body[fields[i]]) {
           return response.status(400).send({status: "error", message: `${fields[i]} is required.`});
         }
       }
+      if (!enumStatusField[0].includes(body.status.toString()) && !enumStatusField[1].includes(body.status.toString())) {
+        return response.status(400).send({status: "error", message: `${body.status} is invalid field.`});
+      }
+
     } else if (method === 'PATCH') {
       for (const field in body) {
-        console.log("entrou no for in")
-
         if (!fields.includes(field.toString())) {
           return response.status(400).send({status: "error", message: `${field} is invalid field.`});
         }
+      }
+
+      if (!enumStatusField[0].includes(body.status.toString()) && !enumStatusField[1].includes(body.status.toString())) {
+        return response.status(400).send({status: "error", message: `${body.status} is invalid status field.`});
       }
     }
     await next();
